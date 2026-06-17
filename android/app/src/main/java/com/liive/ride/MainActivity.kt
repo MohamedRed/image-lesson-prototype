737f36b0be03fe93bc96b87479e96f5becdc6a6e
mrr <mohamed-redha@hotmail.fr>
2026-06-17 10:41:16 +0100

    Polish Liive Ride standalone chrome

--- android/app/src/main/java/com/liive/ride/MainActivity.kt
+++ android/app/src/main/java/com/liive/ride/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.liive.ride
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.remember
-import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+        )
         setContent {
             val stateStore = remember { RideStateStore(applicationContext) }
             val viewModel: RideViewModel = viewModel(
